8831f43659f4a0fefbe3fa0d083013c9fca1c518
gg <gianluigi.alari@gmail.com>
2019-11-02 21:28:43 +0100

    Upload files with Drag & Drop or paste from clipboard

---
+++ scripts/ufdbk.js
@@ -0,0 +1,87 @@
+// Now fake the file upload, since GitHub does not handle file uploads
+// and returns a 404
+
+var minSteps = 6,
+    maxSteps = 60,
+    timeBetweenSteps = 100,
+    bytesPerStep = 100000;
+
+var dropzone = null;
+
+
+document.addEventListener("DOMContentLoaded", function() {
+
+  if (Dropzone.instances.length > 0) {
+      Dropzone.instances.forEach((e) => {
+          e.off();
+          e.destroy();
+      });
+  }
+
+  dropzone = new Dropzone('#demo-upload', {
+    previewTemplate: document.querySelector('#preview-template').innerHTML,
+    parallelUploads: 2,
+    thumbnailHeight: 120,
+    thumbnailWidth: 120,
+    maxFilesize: 3,
+    filesizeBase: 1000,
+    thumbnail: function(file, dataUrl) {
+      if (file.previewElement) {
+        file.previewElement.classList.remove("dz-file-preview");
+        var images = file.previewElement.querySelectorAll("[data-dz-thumbnail]");
+        for (var i = 0; i < images.length; i++) {
+          var thumbnailElement = images[i];
+          thumbnailElement.alt = file.name;
+          thumbnailElement.src = dataUrl;
+        }
+        setTimeout(function() { file.previewElement.classList.add("dz-image-preview"); }, 1);
+      }
+    }
+
+  });
+
+  dropzone.uploadFiles = function(files) {
+    var self = this;
+
+    for (var i = 0; i < files.length; i++) {
+
+      var file = files[i];
+      totalSteps = Math.round(Math.min(maxSteps, Math.max(minSteps, file.size / bytesPerStep)));
+
+      for (var step = 0; step < totalSteps; step++) {
+        var duration = timeBetweenSteps * (step + 1);
+        setTimeout(function(file, totalSteps, step) {
+          return function() {
+            file.upload = {
+              progress: 100 * (step + 1) / totalSteps,
+              total: file.size,
+              bytesSent: (step + 1) * file.size / totalSteps
+            };
+
+            self.emit('uploadprogress', file, file.upload.progress, file.upload.bytesSent);
+            if (file.upload.progress == 100) {
+              file.status = Dropzone.SUCCESS;
+              self.emit("success", file, 'success', null);
+              self.emit("complete", file);
+              self.processQueue();
+              //document.getElementsByClassName("dz-success-mark").style.opacity = "1";
+            }
+          };
+        }(file, totalSteps, step), duration);
+      }
+    }
+  }
+  // Add the pase event listener to the drop zone getElementsByClassName
+  document.querySelector('#dropzone').addEventListener("paste", function(event){
+    var items = (event.clipboardData || event.originalEvent.clipboardData).items;
+    for (index in items) {
+      var item = items[index];
+      if (item.kind === 'file') {
+        // adds the file to your dropzone instance
+        const file = item.getAsFile();
+        if (file != null)
+          dropzone.addFile(item.getAsFile())
+      }
+    }
+  });
+})
